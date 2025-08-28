e12a05b922a34424bf3155cda55b132a1f001cba
Zafer İntepe <108065413+IntepeZafer@users.noreply.github.com>
2025-08-28 15:22:31 +0300

    TestScript Commit
    Test Adın da bir script oluşturuldu ve bunun üzerinde değişiklik yapıldı

---
+++ Assets/Scripts/TestingScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class TestingScript : MonoBehaviour
+{
+    int count = 0;
+    float speed = 5.0f;
+    string message = "Hello , Unity!";
+    bool isActive = true;
+    GameObject player;
+
+    void TestFunction()
+    {
+        
+    }
+}
