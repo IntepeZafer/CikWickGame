94870757275f34208ebb88603809e4b557f462fb
Zafer İntepe <108065413+IntepeZafer@users.noreply.github.com>
2025-08-28 23:31:23 +0300

    commit

--- Assets/Scripts/TestingScript.cs
+++ Assets/Scripts/TestingScript.cs
@@ -3,32 +3,52 @@ using UnityEngine;
 
 public class TestingScript : MonoBehaviour
 {
-    public int myInt = 5;
-
+    int number = 5;
     void Awake() // TODO:Oyun Başlamadan Önce Çalışır
     {
         Debug.Log("Awake Çalıştı");
-        Debug.Log("myInt Değeri: " + myInt);
+        Debug.Log("Normal Debug Log Mesajı");
+        Debug.LogWarning("Uyarı Mesajı");
+        Debug.LogError("Hata Mesajı");
     }
 
-    
+
     void Start() // TODO:  Oyun Başladıktan Sonra Çalışır
     {
-        Debug.Log("Awake Çalıştı"); 
-        Debug.Log("myInt Değeri: " + myInt); 
+        Debug.Log("Start Çalıştı");
+        myFunction();
     }
 
     void Update() // TODO: Her Frame'de Bir Kez Çalışır
     {
-
+        Debug.Log("Update Çalıştı");
     }
-    
+
     private void FixedUpdate() // TODO: Fizik İşlemleri İçin Kullanılır
     {
-
+        Debug.Log("FixedUpdate Çalıştı");
     }
     void LateUpdate() // TODO: Update'den Sonra Çalışır
     {
-        
+        Debug.Log("LateUpdate Çalıştı");
+    }
+    void myFunction()
+    {
+        if (number > 10)
+        {
+            Debug.Log("Sayı 10'dan Büyük");
+        }
+        else if (number < 10)
+        {
+            Debug.Log("Sayı 10'dan Küçük");
+        }
+        else if (number == 10)
+        {
+            Debug.Log("Sayı 10'a Eşit");
+        }
+        else
+        {
+            Debug.Log("Bilinmeyen Durum");
+        }
     }
 }
